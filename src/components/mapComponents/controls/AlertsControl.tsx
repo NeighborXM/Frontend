a350bc99e1c1e94f92e3ab66b8a30866761a6d89
Sean Combs <seanmxd@gmail.com>
2020-04-08 16:06:08 -0400

    Added geolocation

--- src/components/mapComponents/controls/AlertsControl.tsx
+++ src/components/mapComponents/controls/AlertsControl.tsx
@@ -11,13 +11,13 @@ class AlertsControl extends Component<Props> {
         return (
         <div id="alertsControl">
           Click an icon to drop it on the map.<br/>
-          <img alt="I'm okay" id="circleIcon" onClick={() => this.props.mapComponent.dropLogo("circle")} src={icons.circle}/>
+          <img alt="I'm okay" id="circleIcon" onClick={() => this.props.mapComponent.dropIconOntoMap("circle")} src={icons.circle}/>
           I'm ok
-          <img alt="I need help" onClick={() => this.props.mapComponent.dropLogo("exclamation")} src={icons.exclamation}/>
+          <img alt="I need help" onClick={() => this.props.mapComponent.dropIconOntoMap("exclamation")} src={icons.exclamation}/>
           I need help
-          <img alt="I need help soon" onClick={() => this.props.mapComponent.dropLogo("exclamationCircle")} src={icons.exclamationCircle}/>
+          <img alt="I need help soon" onClick={() => this.props.mapComponent.dropIconOntoMap("exclamationCircle")} src={icons.exclamationCircle}/>
           I need help soon
-          <img alt="I need help now" onClick={() => this.props.mapComponent.dropLogo("exclamationTriangle")} src={icons.exclamationTriangle}/>
+          <img alt="I need help now" onClick={() => this.props.mapComponent.dropIconOntoMap("exclamationTriangle")} src={icons.exclamationTriangle}/>
           I need help now
         </div>
         )
